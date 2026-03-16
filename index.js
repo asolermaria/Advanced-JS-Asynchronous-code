d62acc3c589e0dcde7d9d616df9f08faa8a24d7c
asolermaria <asolermaria@gmail.com>
2026-03-16 17:27:28 +0100

    Done ex 1-7

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 //RESUELVE TUS EJERCICIOS AQUI
+
+// Ejercicio 1
+function getAllBreeds() {
+  return fetch("https://dog.ceo/api/breeds/list/all")
+    .then((res) => res.json())
+    .then((data) => {
+      return Object.keys(data.message);
+    });
+}
+
+// Ejercicio 2
+function getRandomDog() {
+  return fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+      return data.message;
+    });
+}
+
+// Ejercicio 3
+function getAllImagesByBreed() {
+  return fetch("https://dog.ceo/api/breed/komondor/images/")
+    .then((res) => res.json())
+    .then((data) => {
+      return data.message;
+    });
+}
+// getAllImagesByBreed().then((data) => console.log(data));
+
+// Ejercicio 4
+function getAllImagesByBreed2(raza) {
+  return fetch(`https://dog.ceo/api/breed/${raza}/images/`)
+    .then((res) => res.json())
+    .then((data) => {
+      return data.message;
+    });
+}
+// getAllImagesByBreed2("dalmatian").then((data) => console.log(data));
+
+// Ejercicio 5
+function getGitHubUserProfile(usuario) {
+  return fetch(`https://api.github.com/users/${usuario}`)
+    .then((res) => res.json())
+    .then((data) => data);
+}
+// getGitHubUserProfile("asolermaria").then((data) => console.log(data));
+
+// Ejercicio 6
+function getGitHubUserProfile(usuario) {
+  return fetch(`https://api.github.com/users/${usuario}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const perfil = {
+        img: data.avatar_url,
+        name: data.name,
+      };
+
+      const container = document.getElementById("perfil-github");
+      container.innerHTML = `<img src="${data.avatar_url}">
+                            <p>${data.name}</p>`;
+      return perfil;
+    });
+}
